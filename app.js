bf02df22c396ecc30953ea8e24b834afa4b84a27
Pham <kristie.pham@hubinternational.com>
2022-07-26 15:01:34 -0400

    First real commit

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// hello.
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+const container = document.querySelector('#container');
+
+for (let i = 1; i < 152; i++) {
+    const pokemon = document.createElement('div');
+    pokemon.classList.add('pokemon');
+    const label = document.createElement('span');
+    label.innerText = `#${i}`;
+    const newImg = document.createElement('img');
+    newImg.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png`;
+    pokemon.appendChild(newImg);
+    pokemon.appendChild(label);
+    container.appendChild(pokemon);
+};
