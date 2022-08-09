1501823ab780dd0ea662a9a042b3c46590f518d5
Pham <kristie.pham@outlook.com>
2022-08-09 09:40:46 -0400

    Refactored js

--- Score Keeper/app.js
+++ Score Keeper/app.js
@@ -7,48 +7,39 @@ for (let i = 5; i < 11; i++) {
     dropdown.appendChild(playToOption);
 }
 
-const p1Btn = document.querySelector('#p1');
-const p2Btn = document.querySelector('#p2');
-const resetBtn = document.querySelector('#reset');
-const p1Score = document.querySelector('#p1Score');
-const p2Score = document.querySelector('#p2Score');
+class Player {
+    constructor(buttonId, scoreId) {
+        this.button = document.querySelector(buttonId);
+        this.score = document.querySelector(scoreId);
+    }
+}
 
-const playerWon = () => {
-    if (p1Score.innerText === dropdown.value || p2Score.innerText === dropdown.value) {
-        if (p1Score.innerText > p2Score.innerText) {
-            p1Score.style.color = 'green';
-            p2Score.style.color = 'red'; 
-        } else {
-            p1Score.style.color = 'red';
-            p2Score.style.color = 'green'; 
-        }
-        p1Btn.disabled = true;
-        p2Btn.disabled = true;
-        p1Btn.style.opacity = 0.5;
-        p2Btn.style.opacity = 0.5;
+const p1 = new Player('#p1', '#p1Score');
+const p2 = new Player('#p2', '#p2Score');
+const resetBtn = document.querySelector('#reset');
+
+const playerScored = (player, opponent) => {
+    player.score.innerText++;
+    if (player.score.innerText === dropdown.value) {
+        player.score.style.color = 'green';
+        opponent.score.style.color = 'red';
+        player.button.disabled = true;
+        opponent.button.disabled = true;
+        player.button.style.opacity = 0.5;
+        opponent.button.style.opacity = 0.5;
     }
 }
 
 const reset = () => {
-    p1Score.innerText = 0;
-    p2Score.innerText = 0;
-    p1Score.style.color = 'black';
-    p2Score.style.color = 'black';
-    p1Btn.disabled = false;
-    p2Btn.disabled = false;
-    p1Btn.style.opacity = 1;
-    p2Btn.style.opacity = 1;
+    for (let player of [p1, p2]) {;
+        player.score.innerText = 0;
+        player.score.style.color = 'black';
+        player.button.disabled = false;
+        player.button.style.opacity = 1;
+    }
 }
 
-p1Btn.addEventListener('click', () => {
-    p1Score.innerText++;
-    playerWon();
-});
-
-p2Btn.addEventListener('click', () => {
-    p2Score.innerText++;
-    playerWon();
-});
-
+p1.button.addEventListener('click', () => playerScored(p1, p2));
+p2.button.addEventListener('click', () => playerScored(p2, p1));
 resetBtn.addEventListener('click', reset);
 dropdown.addEventListener('change', reset);
